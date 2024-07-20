f565f5d81d3eb00ff9da7c014038aa8c5cd29afd
letoast <ciroman@gmail.com>
2024-07-20 23:22:46 +0200

    fix: wip

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,6 +1,8 @@
 import { createDirectus, rest, readItem, readItems, readSingleton } from '@directus/sdk'
+import type { CustomDirectusTypes } from '../types'
 
-const directus = createDirectus('https://svetlikus.datalog.si').with(rest())
+
+const directus = createDirectus<CustomDirectusTypes>('https://svetlikus.datalog.si').with(rest())
 
 export default defineNuxtPlugin(async () => {
 	return {
