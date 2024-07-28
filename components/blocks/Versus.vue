5c389265f68e482e80ce9ead8fad5cd4eebf1153
letoast <ciroman@gmail.com>
2024-07-28 13:47:56 +0200

    fixes

---
+++ components/blocks/Versus.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ImgComparisonSlider } from '@img-comparison-slider/vue'
+
+defineProps<{
+	data: unknown
+	container: boolean
+}>()
+
+const { $directus } = useNuxtApp()
+</script>
+
+<template>
+	<div>
+		<ImgComparisonSlider
+			class="
+				h-auto w-full
+				focus:outline-none
+			"
+		>
+			<!-- eslint-disable -->
+				<img
+					slot="first"
+					class="w-full"
+					:src="`${$directus.url}assets/${data?.before?.id}?format=auto`"
+					:alt="data?.before?.title"
+				>
+				<img
+					slot="second"
+					class="w-full"
+					:src="`${$directus.url}assets/${data?.after?.id}?format=auto`"
+					:alt="data?.after?.title"
+				>
+			<!-- eslint-enable -->
+		</ImgComparisonSlider>
+	</div>
+</template>
